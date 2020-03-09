4d70bd37b2b7afb6ba456c1fe5e5bf6064dd75cb
ErickNaunay <43822508+ErickNaunay@users.noreply.github.com>
2020-03-08 23:36:57 -0500

    Add files via upload

---
+++ dr_arp_discovery.py
@@ -0,0 +1,48 @@
+#This code is based on https://null-byte.wonderhowto.com/how-to/build-arp-scanner-using-scapy-and-python-0162731/
+import sys
+import time
+from datetime import datetime
+from scapy.all import srp, Ether, ARP, conf
+
+lastname_1 = 'NAUNAY'
+lastname_2 = 'CAZCO'
+delimiter = '_'
+scanMode_1 = 'LAN'
+scanMode_2 = 'WLAN'
+extension = '.txt'
+
+if __name__ == "__main__":
+
+    try:
+        interface = input("Enter desired interface: ")
+        ips = input("Enter range of IPs to Scan for: ")
+        timeout_max = int(input("Enter desire timeout (15,30,60):"))
+
+    except KeyboardInterrupt:
+        print ("\n User requested shutdown")
+        print ("Quiting...")
+        sys.exit(1)
+
+    print ("\n Scanning... ")
+
+    timeStamp = time.time()
+    strTimeStamp = datetime.fromtimestamp(timeStamp).strftime('%Y.%m.%d.%H.%M.%S')
+
+    start_time = datetime.now()
+
+    fd = open(lastname_2+delimiter+lastname_1+delimiter+scanMode_1+delimiter+strTimeStamp+extension, 'w')
+
+    conf.verb = 0
+    ans, unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ips), timeout=timeout_max, iface=interface, verbose=False)
+
+    #print ans.summary()
+    print ('MAC - IP')
+    for s, r in ans:
+        print (r.sprintf(r'%Ether.src% - %ARP.psrc%'))
+
+    stop_time = datetime.now()
+    total_time = stop_time - start_time
+
+    print ("\n Scan Complete!")
+    print ("Scan Duration: %s" %(total_time))
+
